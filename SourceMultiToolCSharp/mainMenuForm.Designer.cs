8dc1c2b4d68f3923ed3681e0c079134289124ba1
Jesse Culver <jesseculver92@gmail.com>
2018-07-14 16:34:24 -0500

    Added Menu, Steam finding, setting saving

---
+++ SourceMultiToolCSharp/mainMenuForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SourceMultiToolCSharp
+{
+    partial class mainMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolsPage = new System.Windows.Forms.TabPage();
+            this.buttonModelViewer = new System.Windows.Forms.Button();
+            this.buttonHammer = new System.Windows.Forms.Button();
+            this.settingsPage = new System.Windows.Forms.TabPage();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.richTextBoxAdditionalSteamDirectory = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonBrowseSteamDir = new System.Windows.Forms.Button();
+            this.textSteamDirectory = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.folderBrowserDialogSteam = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.toolsPage.SuspendLayout();
+            this.settingsPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.toolsPage);
+            this.tabControl1.Controls.Add(this.settingsPage);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(507, 436);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // toolsPage
+            // 
+            this.toolsPage.Controls.Add(this.buttonModelViewer);
+            this.toolsPage.Controls.Add(this.buttonHammer);
+            this.toolsPage.Location = new System.Drawing.Point(4, 22);
+            this.toolsPage.Name = "toolsPage";
+            this.toolsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.toolsPage.Size = new System.Drawing.Size(499, 410);
+            this.toolsPage.TabIndex = 0;
+            this.toolsPage.Text = "Tools";
+            this.toolsPage.UseVisualStyleBackColor = true;
+            // 
+            // buttonModelViewer
+            // 
+            this.buttonModelViewer.Location = new System.Drawing.Point(6, 35);
+            this.buttonModelViewer.Name = "buttonModelViewer";
+            this.buttonModelViewer.Size = new System.Drawing.Size(117, 23);
+            this.buttonModelViewer.TabIndex = 1;
+            this.buttonModelViewer.Text = "Model Viewer";
+            this.buttonModelViewer.UseVisualStyleBackColor = true;
+            // 
+            // buttonHammer
+            // 
+            this.buttonHammer.Location = new System.Drawing.Point(6, 6);
+            this.buttonHammer.Name = "buttonHammer";
+            this.buttonHammer.Size = new System.Drawing.Size(117, 23);
+            this.buttonHammer.TabIndex = 0;
+            this.buttonHammer.Text = "Hammer Editor";
+            this.buttonHammer.UseVisualStyleBackColor = true;
+            // 
+            // settingsPage
+            // 
+            this.settingsPage.Controls.Add(this.buttonSave);
+            this.settingsPage.Controls.Add(this.richTextBoxAdditionalSteamDirectory);
+            this.settingsPage.Controls.Add(this.label3);
+            this.settingsPage.Controls.Add(this.buttonBrowseSteamDir);
+            this.settingsPage.Controls.Add(this.textSteamDirectory);
+            this.settingsPage.Controls.Add(this.label2);
+            this.settingsPage.Location = new System.Drawing.Point(4, 22);
+            this.settingsPage.Name = "settingsPage";
+            this.settingsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.settingsPage.Size = new System.Drawing.Size(499, 410);
+            this.settingsPage.TabIndex = 1;
+            this.settingsPage.Text = "Settings";
+            this.settingsPage.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(418, 381);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // richTextBoxAdditionalSteamDirectory
+            // 
+            this.richTextBoxAdditionalSteamDirectory.Location = new System.Drawing.Point(11, 59);
+            this.richTextBoxAdditionalSteamDirectory.Name = "richTextBoxAdditionalSteamDirectory";
+            this.richTextBoxAdditionalSteamDirectory.ReadOnly = true;
+            this.richTextBoxAdditionalSteamDirectory.Size = new System.Drawing.Size(482, 96);
+            this.richTextBoxAdditionalSteamDirectory.TabIndex = 4;
+            this.richTextBoxAdditionalSteamDirectory.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Additional Steam Directories";
+            // 
+            // buttonBrowseSteamDir
+            // 
+            this.buttonBrowseSteamDir.Location = new System.Drawing.Point(418, 6);
+            this.buttonBrowseSteamDir.Name = "buttonBrowseSteamDir";
+            this.buttonBrowseSteamDir.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowseSteamDir.TabIndex = 2;
+            this.buttonBrowseSteamDir.Text = "Browse";
+            this.buttonBrowseSteamDir.UseVisualStyleBackColor = true;
+            this.buttonBrowseSteamDir.Click += new System.EventHandler(this.buttonBrowseSteamDir_Click);
+            // 
+            // textSteamDirectory
+            // 
+            this.textSteamDirectory.Location = new System.Drawing.Point(122, 7);
+            this.textSteamDirectory.Name = "textSteamDirectory";
+            this.textSteamDirectory.Size = new System.Drawing.Size(290, 20);
+            this.textSteamDirectory.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Main Steam Directory";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game/Mod";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 454);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(382, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // folderBrowserDialogSteam
+            // 
+            this.folderBrowserDialogSteam.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // mainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 486);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "mainMenuForm";
+            this.Text = "Source MultiTool";
+            this.Load += new System.EventHandler(this.mainMenuForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.toolsPage.ResumeLayout(false);
+            this.settingsPage.ResumeLayout(false);
+            this.settingsPage.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage toolsPage;
+        private System.Windows.Forms.TabPage settingsPage;
+        private System.Windows.Forms.Button buttonModelViewer;
+        private System.Windows.Forms.Button buttonHammer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxAdditionalSteamDirectory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonBrowseSteamDir;
+        private System.Windows.Forms.TextBox textSteamDirectory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogSteam;
+    }
+}
---
+++ SourceMultiToolCSharp/mainMenuForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SourceMultiToolCSharp
+{
+    public partial class mainMenuForm : Form
+    {
+        public mainMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowseSteamDir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialogSteam.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                textSteamDirectory.Text = folderBrowserDialogSteam.SelectedPath;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textSteamDirectory.Text+"/Steam.exe"))
+            {
+                Properties.Settings.Default.mainSteamDir = textSteamDirectory.Text;
+                findSteamDirectories();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("The Main Steam Directory you have chosen does not contain Steam.exe meaning it is not your main directory","ERROR 001",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+
+        private void mainMenuForm_Load(object sender, EventArgs e)
+        {
+            textSteamDirectory.Text = Properties.Settings.Default.mainSteamDir;
+        }
+
+        private void findSteamDirectories()
+        {
+            if (File.Exists(textSteamDirectory.Text + "/steamapps/libraryfolders.vdf"))
+            {
+                string[] lines = File.ReadAllLines(textSteamDirectory.Text + "/steamapps/libraryfolders.vdf");
+                richTextBoxAdditionalSteamDirectory.Text = lines[4];
+            }
+        }
+    }
+}
